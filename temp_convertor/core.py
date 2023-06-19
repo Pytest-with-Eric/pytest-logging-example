5de433ae1135206f507bcbc24e4fa1676957b7b4
Eric Sales De Andrade <eric.salesdeandrade@joinstudyhall.com>
2023-06-19 10:21:59 +0100

    Code fixes

--- temp_convertor/core.py
+++ temp_convertor/core.py
@@ -1,5 +1,6 @@
 import logging
 
+# Set up Logging
 # Create a named logger
 logger = logging.getLogger('__temp_convertor__')
 logger.setLevel(logging.INFO)
@@ -16,7 +17,20 @@ console_handler.setFormatter(formatter)
 # Add the console handler to the logger
 logger.addHandler(console_handler)
 
+# Create a file handler
+file_handler = logging.FileHandler('./logs/temp_convertor_src_run.log')
+file_handler.setLevel(logging.INFO)
 
+# Set the formatter for the file handler
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                                datefmt='%m/%d/%Y %I:%M:%S%p')
+file_handler.setFormatter(formatter)
+
+# Add the file handler to the logger
+logger.addHandler(file_handler)
+
+
+############################ Source code ############################ 
 def fahrenheit_to_celsius(fahrenheit: float) -> float:
     """
     Convert the specified Fahrenheit temperature to Celsius and return it.
@@ -38,6 +52,6 @@ def celsius_to_fahrenheit(celsius: float) -> float:
     logger.info(f"Result: {fahrenheit}°F")
     return fahrenheit
 
-# if __name__ == "__main__":
-#     fahrenheit_to_celsius(90)
-#     celsius_to_fahrenheit(19)
+if __name__ == "__main__":
+    fahrenheit_to_celsius(90)
+    celsius_to_fahrenheit(19)
--- tests/unit/test_temp_convertor.py
+++ tests/unit/test_temp_convertor.py
@@ -1,7 +1,8 @@
 import logging
+import pytest
 from temp_convertor.core import fahrenheit_to_celsius, celsius_to_fahrenheit
 
-# Create a named logger
+# Get logger
 logger = logging.getLogger('__temp_convertor__')
 logger.setLevel(logging.DEBUG)
 
@@ -12,5 +13,11 @@ def test_fahrenheit_to_celsius():
 
 def test_celsius_to_fahrenheit():
     assert celsius_to_fahrenheit(19) == 66.2
+
+def test_celecius_to_fahrenheit_caplog_ex(caplog):
+    caplog.set_level(logging.DEBUG, logger='__temp_convertor__') # Overwrite the default log level using caplog fixture
+    assert celsius_to_fahrenheit(300) == 572.0
+    for record in caplog.records:  # Print Caplog records
+        print(record.levelname, record.message)
 
 
