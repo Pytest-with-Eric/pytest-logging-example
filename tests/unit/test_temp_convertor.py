4a6a42e309d11fbdc926e4941797451edcca083e
Eric Sales De Andrade <eric.salesdeandrade@joinstudyhall.com>
2023-06-20 09:44:32 +0100

    Typo

--- tests/unit/test_temp_convertor.py
+++ tests/unit/test_temp_convertor.py
@@ -1,5 +1,4 @@
 import logging
-import pytest
 from temp_convertor.core import fahrenheit_to_celsius, celsius_to_fahrenheit
 
 # Get logger
@@ -14,7 +13,7 @@ def test_fahrenheit_to_celsius():
 def test_celsius_to_fahrenheit():
     assert celsius_to_fahrenheit(19) == 66.2
 
-def test_celecius_to_fahrenheit_caplog_ex(caplog):
+def test_celsius_to_fahrenheit_caplog_ex(caplog):
     caplog.set_level(logging.DEBUG, logger='__temp_convertor__') # Overwrite the default log level using caplog fixture
     assert celsius_to_fahrenheit(300) == 572.0
     for record in caplog.records:  # Print Caplog records
